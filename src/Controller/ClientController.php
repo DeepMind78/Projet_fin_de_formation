f73e4df49a72701ecbea0f95c7a8c923a72f3ae6
Sullivan78 <cullellsullivan@gmail.com>
2020-01-07 16:14:51 +0100

    finition des pages de rdv infos full

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Client;
 use App\Form\ClientType;
 use App\Repository\ClientRepository;
+use App\Repository\CoachRepository;
 use App\Repository\RdvRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -49,13 +50,18 @@ class ClientController extends AbstractController
      * @Route ("/rdvClient", name="client.rdv")
      */
 
-    public function rdvclient(ClientRepository $repoClient, RdvRepository $repoRdv) {
+    public function rdvclient(ClientRepository $repoClient, RdvRepository $repoRdv, CoachRepository $repoCoach) {
         $user = $this->getUser()->getId();
         $resultat = $repoClient->findBy(['user'=>$user]);
         $idclient = $resultat[0]->getId();
         $rdv = $repoRdv->findBy(['client'=> $idclient]);
-        dump($idclient);
-        dump($rdv);
+        //dump($idclient);
+        //dump($rdv);
+
+        //$coach = $repoRdv->findBy('coach_id')
+        //$resultat1 = $repoCoach->findBy(['user'=>$user]);
+        //$nomcoach = $resultat1[0]->getId();
+        //dump($nomcoach);
 
         return $this->render('/rdv/rdvclient.html.twig', [
             'rdvclients' => $rdv
